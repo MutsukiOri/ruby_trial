722f57885016e37290a8958d936086bfa437affb
MutsukiOri <ori.mutsuki@arakawa-lab.com>
2025-04-15 14:41:39 +0900

    4-5 add create_cart

--- techpit-clothes/app/controllers/application_controller.rb
+++ techpit-clothes/app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :create_cart
+
+  private
+
+  def create_cart
+    return if session[:cart_id]
+
+    cart = Cart.create
+    session[:cart_id] = cart.id
+  end
 end
