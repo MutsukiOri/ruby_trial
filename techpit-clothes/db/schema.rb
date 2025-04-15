eacf5bca71b2f0faef923f8cd6363a5397cf182d
MutsukiOri <ori.mutsuki@arakawa-lab.com>
2025-04-15 14:22:38 +0900

    4-3 define tables (Cart, line_item)

--- techpit-clothes/db/schema.rb
+++ techpit-clothes/db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_04_15_042427) do
+ActiveRecord::Schema[7.0].define(version: 2025_04_15_051931) do
+  create_table "carts", charset: "utf8mb4", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "line_items", charset: "utf8mb4", force: :cascade do |t|
+    t.bigint "product_id", null: false
+    t.bigint "cart_id", null: false
+    t.integer "quantity", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_line_items_on_cart_id"
+    t.index ["product_id"], name: "index_line_items_on_product_id"
+  end
+
   create_table "products", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", null: false
     t.text "description", null: false
@@ -20,4 +35,6 @@ ActiveRecord::Schema[7.0].define(version: 2025_04_15_042427) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "line_items", "carts"
+  add_foreign_key "line_items", "products"
 end
