9761b23df80e369e69d86662c45d4be43876a5f7
MutsukiOri <ori.mutsuki@arakawa-lab.com>
2025-04-16 13:27:04 +0900

    5-9

--- techpit-clothes/app/controllers/payments_controller.rb
+++ techpit-clothes/app/controllers/payments_controller.rb
@@ -7,29 +7,7 @@ class PaymentsController < ApplicationController
   def checkout
     redirect_to carts_path and return if @cart.line_items.empty?
 
-    line_items = []
-
-    @cart.line_items.each do |line_item|
-      line_items << {
-        price_data: {
-          unit_amount: line_item.product.price,
-          currency: 'jpy',
-          product_data: {
-            name: line_item.product.name,
-            description: line_item.product.description
-          }
-        },
-        quantity: line_item.quantity
-      }
-    end
-
-    @session = Stripe::Checkout::Session.create({
-                                                  payment_method_types: ['card'],
-                                                  line_items: line_items,
-                                                  mode: 'payment',
-                                                  success_url: payments_success_url,
-                                                  cancel_url: carts_url
-                                                })
+    @session = Stripe::Checkout::Session.create(StripeCheckout.make_stripe_checkout_params(cart: @cart))
   end
 
   def success
